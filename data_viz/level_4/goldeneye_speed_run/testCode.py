12d23f6fe0fa18b3fcbdf0d377c481189a100ef4
Aaron Fox <aaron.f@deakin.edu.au>
2023-02-07 14:27:13 +1100

    Bulk push for level 3 & 4 (unfinished viz).

---
+++ data_viz/level_4/goldeneye_speed_run/testCode.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: 
+    Aaron Fox
+    Centre for Sport Research
+    Deakin University
+    aaron.f@deakin.edu.au
+    
+"""
+
+# %% Import packages
+
+import pandas as pd
+
+
+
+# %% Set-up
+
+### TODO: add any set-up code here
+
+# #Set matplotlib parameters
+# from matplotlib import rcParams
+# # rcParams['font.family'] = 'sans-serif'
+# rcParams['font.sans-serif'] = 'Arial'
+# rcParams['font.weight'] = 'bold'
+# rcParams['axes.labelsize'] = 12
+# rcParams['axes.titlesize'] = 16
+# rcParams['axes.linewidth'] = 1.5
+# rcParams['axes.labelweight'] = 'bold'
+# rcParams['legend.fontsize'] = 10
+# rcParams['xtick.major.width'] = 1.5
+# rcParams['ytick.major.width'] = 1.5
+# rcParams['legend.framealpha'] = 0.0
+# rcParams['savefig.dpi'] = 300
+# rcParams['savefig.format'] = 'pdf'
+
+# %%
+
+
+# %% ----- End of testCode.py -----
